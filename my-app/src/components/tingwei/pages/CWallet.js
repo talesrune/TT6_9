f938a971d497f5fad4e803e9f651bdf40e026cbd
Ting Wei <ltwei96@gmail.com>
2022-07-22 22:58:17 +0800

    Finally figured out how to display interesting json from api

--- my-app/src/components/tingwei/pages/CWallet.js
+++ my-app/src/components/tingwei/pages/CWallet.js
@@ -5,37 +5,75 @@ import axios from 'axios';
 const C_Wallet = () => {
 
   const [wallet_id, setWalletId] = useState('')
-  const [cWallets, setCWallets] = useState(
-    [
+  const [cWallets, setCWallets] = useState(//[])
+     [
+      {
+        "sgd":"453"
+      },
+      {
+        "nzd":"123"
+      }
       // {
-      //   "sgd":453
+      //   "id": 1,
+      //   "wallet_id": 1,
+      //   "currency": "SGD",
+      //   "sgd":423,
+      //   "amount": 4294.50
       // },
       // {
-      //   "nzd":123
+      //   "id": 2,
+      //   "wallet_id": 1,
+      //   "currency": "CAD",
+      //   "amount": 5687.65
+      // },
+      // {
+      //   "id": 3,
+      //   "wallet_id": 1,
+      //   "currency": "CNH",
+      //   "amount": 6063.14
       // }
-      {
-        "id": 1,
-        "wallet_id": 1,
-        "currency": "SGD",
-        "sgd":423,
-        "amount": 4294.50
-      },
-      {
-        "id": 2,
-        "wallet_id": 1,
-        "currency": "CAD",
-        "amount": 5687.65
-      },
-      {
-        "id": 3,
-        "wallet_id": 1,
-        "currency": "CNH",
-        "amount": 6063.14
-      }
     ])
 
     const get_cwallets = async() => {
-      axios({
+
+      let fake_str = "[('sgd', '423'), ('usd', '123')]"
+
+      let temp_json = fake_str.replaceAll('(', '{').replaceAll(")","}").replaceAll(",",":").replaceAll(": {",", {").replaceAll("'","\"")
+        let fin_json = JSON.parse(temp_json)
+       //  JSON.stringify(temp,null,2);
+       
+
+        console.log(fin_json[0])
+
+        let fin2 = []
+
+        let entry
+
+
+        for (let [key, value] of fin_json.entries()){
+            //console.log(value)
+            
+            let entries = Object.entries(value)
+
+            let data = entries.map( ([key2, val] = entry) => {
+              //fin2[key] = value;
+              let temp = {}
+              temp[key2] = val;
+              
+              fin2.push(temp)
+              return `Currency: ${key2} , Value: ${val}`;
+              
+            });
+            console.log(data)
+        }
+        console.log(fin2)
+        console.log('hi')
+        console.log(cWallets)
+        //setCWallets(fin2)
+        console.log('done')
+        console.log(cWallets)
+
+ /*     axios({
         method: 'get', //you can set what request you want to be
         url: 'http://35.91.201.214/get_wallet?send_to_server=' + '1'
       
@@ -86,25 +124,36 @@ const C_Wallet = () => {
       
     }).catch(function (error) {
         console.log(error);
-      });
+      });*/
   }
 
 
   useEffect(()=>{
     get_cwallets()
   }, []) //this [] makes it runs first render only
+
+  let entry2
   return (
     <div className="wallet-container">
         <div className="c_wallets">
-            {cWallets && cWallets.map((cWallet)=>(
-                <div className='wallet-details' key={cWallet.id}>
-                <h3><strong>ID: </strong>{cWallet.id}</h3>
+            {/* {cWallets &&  cWallets.map( ([key2, val] = entry2)=>(
+                <div className='wallet-details' >
+                <h3><strong>ID: </strong>{entry2}</h3>
+                </div>
+            ))} */}
+            {
+            cWallets && Object.entries(cWallets).map(([key2,value]) =>( 
+                <div className='wallet-details' key={key2}>
+                  {Object.entries(value).map(([key3,value2]) => (
+                    <div key={key3}>{key3} {value2}
+                  </div>
+                  
+                  ))}
+
+                {/* <h3><strong>ID: </strong>{cWallet.id}</h3>
                 <p><strong>Wallet ID:</strong> {cWallet.wallet_id}</p>
                 <p><strong>Currency:</strong> {cWallet.currency}</p>
-                <p><strong>Amount:</strong> {cWallet.amount}</p>
-                {/* <p><strong>Currency:</strong> {cWallet.wallet_id}</p> */}
-                {/* <p>{book.createdAt}</p>
-                <span onClick={()=> deleteBook(book._id)}>delete</span> */}
+                <p><strong>Amount:</strong> {cWallet.amount}</p> */}
                 </div>
             ))}
         </div>
